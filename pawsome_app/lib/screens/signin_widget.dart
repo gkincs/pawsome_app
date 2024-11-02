544a330dee46ea2fa9ce09dd164051e70e7f275c
gkiincs <gazdagkincso@gmail.com>
2024-11-02 10:54:39 +0100

    flutter svg dependency, sign in UI test

---
+++ pawsome_app/lib/screens/signin_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SigninWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max, // A maximális hely kitöltése
+            mainAxisAlignment: MainAxisAlignment.center, // Középre igazítás
+            crossAxisAlignment: CrossAxisAlignment.stretch, // Teljes szélesség kitöltése
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
+                  child: Text(
+                    'PawSome',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 100), // Hely a PawSome és a Sign in között
+              ElevatedButton(
+                onPressed: () {
+                  // Sign in gomb esemény
+                },
+                child: Text('Sign in'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 48), // Gomb szélessége
+                ),
+              ),
+              SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  // Create account gomb esemény
+                },
+                child: Text('Create an account'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
